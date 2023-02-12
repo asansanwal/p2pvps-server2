a858f880d4e9fd20e7cb1b8b8812cf7749905c50
K. Singh <95389941+asansanwal@users.noreply.github.com>
2023-02-12 06:47:15 -0500

    Update local.js

--- config/env/local.js
+++ config/env/local.js
@@ -11,5 +11,5 @@ module.exports = {
   token: 'secret-jwt-token',
 
   // Used for local development.
-  database: 'mongodb://localhost:27017/p2pvps-server-local'
+  database: 'mongodb://serverdeployment2_mongodb_1:27017/p2pvps-server-local'
 }
