4df2d813e7fe1f3cecc1879970e5c60fa2cb2051
K. Singh <95389941+asansanwal@users.noreply.github.com>
2023-02-12 07:02:52 -0500

    Update test.js

--- config/env/test.js
+++ config/env/test.js
@@ -2,12 +2,12 @@
 export default {
   session: 'secret-boilerplate-token',
   token: 'secret-jwt-token',
-  database: 'mongodb://localhost:27017/p2pvps-server-test'
+  database: 'mongodb://serverdeployment2_mongodb_1:27017/p2pvps-server-test'
 }
 */
 
 module.exports = {
   session: 'secret-boilerplate-token',
   token: 'secret-jwt-token',
-  database: 'mongodb://localhost:27017/p2pvps-server-test'
+  database: 'mongodb://serverdeployment2_mongodb_1:27017/p2pvps-server-test'
 }
