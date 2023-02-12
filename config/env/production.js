430cb82fae79e93b5b7feea24d50618fd4b3e811
K. Singh <95389941+asansanwal@users.noreply.github.com>
2023-02-12 06:46:50 -0500

    Update production.js

--- config/env/production.js
+++ config/env/production.js
@@ -2,12 +2,12 @@
 export default {
   session: 'secret-boilerplate-token',
   token: 'secret-jwt-token',
-  database: 'mongodb://localhost:27017/p2pvps-server-prod'
+  database: 'mongodb://serverdeployment2_mongodb_1:27017/p2pvps-server-prod'
 }
 */
 
 module.exports = {
   session: 'secret-boilerplate-token',
   token: 'secret-jwt-token',
-  database: 'mongodb://localhost:27017/p2pvps-server-prod'
+  database: 'mongodb://serverdeployment2_mongodb_1:27017/p2pvps-server-prod'
 }
